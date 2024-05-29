de4b4564f3c4a84e2f9dd11b9dcb57d1347a7adc
Jacob Kelly <jskelly2021@gmail.com>
2024-05-29 16:30:49 -0500

    Added basic horizontal and vertical movement

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+    
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+        body.velocity = direction * speed;
+    }
+}
