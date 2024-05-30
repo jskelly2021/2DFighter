c00fba41cdc9cc3c534ab52242eeb58c8a42e3a5
Jacob Kelly <jskelly2021@gmail.com>
2024-05-30 16:56:01 -0500

    Added Jump capability using a LayerMask for the ground

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public Rigidbody2D body;
-    public float speed;
+    [SerializeField] private LayerMask groundLayerMask;
 
+    public Rigidbody2D body;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private BoxCollider2D groundCheck;
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +27,21 @@ public class playerMovement : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2 (xInput * speed, body.velocity.y);
 
+        // Is player grounded
+        if (groundCheck.IsTouchingLayers(groundLayerMask))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
 
     }
 }
