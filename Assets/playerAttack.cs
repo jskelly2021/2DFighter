5121d3729f3efbbc1643dd2f4f9fe36cb8657956
Jacob Kelly <jskelly2021@gmail.com>
2024-06-01 15:27:56 -0500

    Wrote logic for animation que in playerAttack.cs

--- Assets/playerAttack.cs
+++ Assets/playerAttack.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playerAttack : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,36 @@ public class playerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 
+        // Determine Attack from input
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AttackFront();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            AttackBack();
+        }
+        else if (!Input.GetKeyDown(KeyCode.R))
+        {
+            AttackOverhead();
+        }
     }
+
+    void AttackFront()
+    {
+        animator.SetBool("isAttackFront", true);
+    }
+
+    void AttackBack()
+    {
+        animator.SetBool("isAttackBack", true);
+
+    }
+
+    void AttackOverhead()
+    {
+        animator.SetBool("isAttackOverhead", true);
+    }
+
+
 }
