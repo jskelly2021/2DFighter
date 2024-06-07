0b8f54cd9db4b4b0691ab0f8249dce4b571adf0e
Jacob Kelly <jskelly2021@gmail.com>
2024-06-07 12:04:52 -0500

    When dead, if player jumps, the character will move to jumping state. Only for testing purposes

--- Assets/Scripts/Movement/MoveStates/DeadStateMoveAction.cs
+++ Assets/Scripts/Movement/MoveStates/DeadStateMoveAction.cs
@@ -3,5 +3,13 @@ using UnityEngine;
 
 public class DeadStateMoveAction : CharacterState
 {
-    
+
+    // Only for testing
+    public override void Jump()
+    {
+        if (character.IsJumping())
+        {
+            character.SetCharacterState(characterState.jump);
+        }
+    }
 }
