f05461812f9ca78f742a6e2407940fc29d18f3b8
Jacob Kelly <jskelly2021@gmail.com>
2024-06-12 12:48:38 -0500

    Removed stun timer when character is hurt

--- Assets/Scripts/CharacterState/CharacterStates/HurtState.cs
+++ Assets/Scripts/CharacterState/CharacterStates/HurtState.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class HurtState : BaseCharacterState
 {
-    private void OnEnable()
-    {
-        StartCoroutine(StunTimer());
-    }
-
-    IEnumerator StunTimer()
-    {
-        yield return new WaitForSeconds(character.StunTime);
-        
-        if (character.GetCharacterState() != CharacterState.Dead)
-        {
-            character.SetCharacterState(CharacterState.Idle);
-        }
-    }
+   
 }
