3099baba92ec4ab1d36c2a8c9ccef75c7fea3dfc
Jacob Kelly <jskelly2021@gmail.com>
2024-06-22 13:37:38 -0500

    Removed characterBase and added characterStats to hold stats for the character

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,7 +1,7 @@
 
 using UnityEngine;
 
-public class CharacterBase : MonoBehaviour
+public class CharacterStats : MonoBehaviour
 {
     public Rigidbody2D body;
     public BoxCollider2D groundCheck;
